4134e2014c6438d0e12f094839826aa01da8a9de
Hao Bui <haobp.dev@gmail.com>
2024-04-10 00:44:00 -0500

    add id to the user session, change user avatar to user image if there is one

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ import { useContext } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md';
 import { useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 const Header = () => {
   const { darkTheme, setDarkTheme } = useContext(ThemeContext);
@@ -20,9 +21,27 @@ const Header = () => {
         </Link>
         <ul className='flex items-center ml-5'>
           <li className='flex items-center'>
-            <Link href='/auth'>
-              <FaUserCircle className='cursor-pointer' />
-            </Link>
+            {session?.user ? (
+              <Link href={`/users/${session.user.id}`}>
+                {session.user.image ? (
+                  <div className='w-10 h-10 rounded-full overflow-hidden'>
+                    <Image
+                      src={session.user.image}
+                      alt={session.user.name!}
+                      width={40}
+                      height={40}
+                      className='scale-animation img'
+                    />
+                  </div>
+                ) : (
+                  <FaUserCircle className='cursor-pointer' />
+                )}
+              </Link>
+            ) : (
+              <Link href='/auth'>
+                <FaUserCircle className='cursor-pointer' />
+              </Link>
+            )}
           </li>
           <li className='ml-2'>
             {darkTheme ? (

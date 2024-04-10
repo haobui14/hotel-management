74a8cd5d8e11e118ba7ab784b2edf545969d010f
Hao Bui <haobp.dev@gmail.com>
2024-04-10 14:40:22 -0500

    create a HeroSection component for homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,3 +1,9 @@
+import HeroSection from '@/components/HeroSection/HeroSection';
+
 export default function Home() {
-  return <>{/* Hero Section */}</>;
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
 }
